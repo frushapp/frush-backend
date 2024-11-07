817bbf07b484e4a16971a5d30db9048b6238f3cc
shaunFrost1234 <nitinsinghchib0@gmail.com>
2024-11-07 11:23:55 +0530

    report_change

--- resources/views/admin-views/report/food-wise-report.blade.php
+++ resources/views/admin-views/report/food-wise-report.blade.php
@@ -152,7 +152,7 @@
                         <tr>
                             <td>{{$row->food_id}}</td>
                             <td><?php echo $row->food_details;?></td>
-                            <td>{{}}</td>
+                            <td></td>
                             <td>{{$row->quantity}}</td>
                             <td>Rs. {{$row->price}}</td>
                             <td>Rs. {{$row->price*$row->quantity}}</td>
