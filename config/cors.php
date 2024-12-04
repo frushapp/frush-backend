af8cd52fdaefa87e97ed10ae2dd05a5a5f859285
NitinFrantic <nitin@franticpro.com>
2024-12-05 01:38:09 +0530

    Update cors.php

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,10 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => [
+        'https://web.tastyfood.buzz', // Flutter web app's subdomain
+        'https://tastyfood.buzz', // Main domain
+    ],
 
     'allowed_origins_patterns' => [],
 
