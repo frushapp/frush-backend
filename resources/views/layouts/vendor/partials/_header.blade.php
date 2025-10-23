9c8062bff9f05679fb17d6219fad25214b62dd26
frantic03117 <shahrukh@franticpro.com>
2025-10-23 17:39:50 +0530

    asset('assets

--- resources/views/layouts/vendor/partials/_header.blade.php
+++ resources/views/layouts/vendor/partials/_header.blade.php
@@ -1,20 +1,19 @@
 <div id="headerMain" class="d-none">
     <header id="header"
-            class="navbar navbar-expand-lg navbar-fixed navbar-height navbar-flush navbar-container navbar-bordered">
+        class="navbar navbar-expand-lg navbar-fixed navbar-height navbar-flush navbar-container navbar-bordered">
         <div class="navbar-nav-wrap">
             <div class="navbar-brand-wrapper">
                 <!-- Logo Div-->
-                @php($restaurant_logo=\App\CentralLogics\Helpers::get_restaurant_data()->logo)
-                <a class="navbar-brand" href="{{route('vendor.dashboard')}}" aria-label="">
+                @php($restaurant_logo = \App\CentralLogics\Helpers::get_restaurant_data()->logo)
+                <a class="navbar-brand" href="{{ route('vendor.dashboard') }}" aria-label="">
                     <img class="navbar-brand-logo"
-                         style="border-radius: 50%;height: 47px;width: 47px!important; border: 5px solid #80808012"
-                         onerror="this.src='{{asset('assets/admin/img/160x160/img1.jpg')}}'"
-                         src="{{asset('storage/app/public/restaurant/'.$restaurant_logo)}}" alt="Logo">
+                        style="border-radius: 50%;height: 47px;width: 47px!important; border: 5px solid #80808012"
+                        onerror="this.src='{{ asset('public/assets/admin/img/160x160/img1.jpg') }}'"
+                        src="{{ asset('storage/app/public/restaurant/' . $restaurant_logo) }}" alt="Logo">
                     <img class="navbar-brand-logo-mini"
-                         style="border-radius: 50%;height: 47px;width: 47px!important; border: 5px solid #80808012"
-                         onerror="this.src='{{asset('assets/admin/img/160x160/img1.jpg')}}'"
-                         src="{{asset('storage/app/public/restaurant/'.$restaurant_logo)}}"
-                         alt="Logo">
+                        style="border-radius: 50%;height: 47px;width: 47px!important; border: 5px solid #80808012"
+                        onerror="this.src='{{ asset('public/assets/admin/img/160x160/img1.jpg') }}'"
+                        src="{{ asset('storage/app/public/restaurant/' . $restaurant_logo) }}" alt="Logo">
                 </a>
                 <!-- End Logo -->
             </div>
@@ -23,10 +22,10 @@
                 <!-- Navbar Vertical Toggle -->
                 <button type="button" class="js-navbar-vertical-aside-toggle-invoker close mr-3">
                     <i class="tio-first-page navbar-vertical-aside-toggle-short-align" data-toggle="tooltip"
-                       data-placement="right" title="Collapse"></i>
+                        data-placement="right" title="Collapse"></i>
                     <i class="tio-last-page navbar-vertical-aside-toggle-full-align"
-                       data-template='<div class="tooltip d-none d-sm-block" role="tooltip"><div class="arrow"></div><div class="tooltip-inner"></div></div>'
-                       data-toggle="tooltip" data-placement="right" title="Expand"></i>
+                        data-template='<div class="tooltip d-none d-sm-block" role="tooltip"><div class="arrow"></div><div class="tooltip-inner"></div></div>'
+                        data-toggle="tooltip" data-placement="right" title="Expand"></i>
                 </button>
                 <!-- End Navbar Vertical Toggle -->
             </div>
@@ -39,9 +38,9 @@
                         <!-- Notification -->
                         <div class="hs-unfold">
                             <a class="js-hs-unfold-invoker btn btn-icon btn-ghost-secondary rounded-circle"
-                               href="{{route('vendor.order.list',['status'=>'pending'])}}">
+                                href="{{ route('vendor.order.list', ['status' => 'pending']) }}">
                                 <i class="tio-shopping-cart-outlined"></i>
-                                {{--<span class="btn-status btn-sm-status btn-status-danger"></span>--}}
+                                {{-- <span class="btn-status btn-sm-status btn-status-danger"></span> --}}
                             </a>
                         </div>
                         <!-- End Notification -->
@@ -51,46 +50,50 @@
                         <!-- Account -->
                         <div class="hs-unfold">
                             <a class="js-hs-unfold-invoker navbar-dropdown-account-wrapper" href="javascript:;"
-                               data-hs-unfold-options='{
+                                data-hs-unfold-options='{
                                      "target": "#accountNavbarDropdown",
                                      "type": "css-animation"
                                    }'>
                                 <div class="avatar avatar-sm avatar-circle">
                                     <img class="avatar-img"
-                                        onerror="this.src='{{asset('public/assets/admin/img/160x160/img1.jpg')}}'"
-                                        src="{{asset('storage/app/public/vendor')}}/{{\App\CentralLogics\Helpers::get_loggedin_user()->image}}"
+                                        onerror="this.src='{{ asset('public/assets/admin/img/160x160/img1.jpg') }}'"
+                                        src="{{ asset('storage/app/public/vendor') }}/{{ \App\CentralLogics\Helpers::get_loggedin_user()->image }}"
                                         alt="Image Description">
                                     <span class="avatar-status avatar-sm-status avatar-status-success"></span>
                                 </div>
                             </a>
 
                             <div id="accountNavbarDropdown"
-                                 class="hs-unfold-content dropdown-unfold dropdown-menu dropdown-menu-right navbar-dropdown-menu navbar-dropdown-account"
-                                 style="width: 16rem;">
+                                class="hs-unfold-content dropdown-unfold dropdown-menu dropdown-menu-right navbar-dropdown-menu navbar-dropdown-account"
+                                style="width: 16rem;">
                                 <div class="dropdown-item-text">
                                     <div class="media align-items-center">
                                         <div class="avatar avatar-sm avatar-circle mr-2">
                                             <img class="avatar-img"
-                                                 onerror="this.src='{{asset('public/assets/admin/img/160x160/img1.jpg')}}'"
-                                                 src="{{asset('storage/app/public/vendor')}}/{{\App\CentralLogics\Helpers::get_loggedin_user()->image}}"
-                                                 alt="Owner image">
+                                                onerror="this.src='{{ asset('public/assets/admin/img/160x160/img1.jpg') }}'"
+                                                src="{{ asset('storage/app/public/vendor') }}/{{ \App\CentralLogics\Helpers::get_loggedin_user()->image }}"
+                                                alt="Owner image">
                                         </div>
                                         <div class="media-body">
-                                            <span class="card-title h5">{{\App\CentralLogics\Helpers::get_loggedin_user()->f_name}}</span>
-                                            <span class="card-text">{{\App\CentralLogics\Helpers::get_loggedin_user()->email}}</span>
+                                            <span
+                                                class="card-title h5">{{ \App\CentralLogics\Helpers::get_loggedin_user()->f_name }}</span>
+                                            <span
+                                                class="card-text">{{ \App\CentralLogics\Helpers::get_loggedin_user()->email }}</span>
                                         </div>
                                     </div>
                                 </div>
 
                                 <div class="dropdown-divider"></div>
 
-                                <a class="dropdown-item" href="{{route('vendor.profile.view')}}">
-                                    <span class="text-truncate pr-2" title="Settings">{{__('messages.settings')}}</span>
+                                <a class="dropdown-item" href="{{ route('vendor.profile.view') }}">
+                                    <span class="text-truncate pr-2"
+                                        title="Settings">{{ __('messages.settings') }}</span>
                                 </a>
 
                                 <div class="dropdown-divider"></div>
 
-                                <a class="dropdown-item" href="javascript:" onclick="Swal.fire({
+                                <a class="dropdown-item" href="javascript:"
+                                    onclick="Swal.fire({
                                     title: 'Do you want to logout?',
                                     showDenyButton: true,
                                     showCancelButton: true,
@@ -100,16 +103,17 @@
                                     denyButtonText: `Don't Logout`,
                                     }).then((result) => {
                                     if (result.value) {
-                                        @if(auth('vendor')->check())
-                                        location.href='{{route('vendor.auth.logout')}}';
-                                        @elseif (auth('vendor_employee')->check())
-                                        location.href='{{route('vendor.auth.employee.logout')}}';
-                                        @endif
+                                        @if (auth('vendor')->check())
+location.href='{{ route('vendor.auth.logout') }}';
+@elseif (auth('vendor_employee')->check())
+location.href='{{ route('vendor.auth.employee.logout') }}';
+@endif
                                     } else{
                                     Swal.fire('Canceled', '', 'info')
                                     }
                                     })">
-                                    <span class="text-truncate pr-2" title="Sign out">{{__('messages.sign_out')}}</span>
+                                    <span class="text-truncate pr-2"
+                                        title="Sign out">{{ __('messages.sign_out') }}</span>
                                 </a>
                             </div>
                         </div>
